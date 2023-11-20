1be192af06faea3490dc063c05052a741edc18ec
Jai <jai.k.ravula@gmail.com>
2023-11-19 22:52:46 -0500

    Initial Commit

---
+++ SMARTAPP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SMARTAPP
+//
+//  Created by Jai Ravula on 11/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SMARTAPP/SMARTAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMARTAPPApp.swift
+//  SMARTAPP
+//
+//  Created by Jai Ravula on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SMARTAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
